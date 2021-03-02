4b7489be0a074f5f96125c864ba2b0c7d205900e
Jan Dale Amores <amoresjan14@gmail.com>
2021-03-03 04:03:34 +0800

    added constants for reserved words

---
+++ CFPL_Interpreter/constants.py
@@ -0,0 +1,18 @@
+RESERVED_KEYWORDS = {
+    'VAR',
+    'AS',
+    'INT',
+    'CHAR',
+    'BOOL',
+    'FLOAT',
+    'START',
+    'STOP',
+    'AND',
+    'OR',
+    'NOT',
+    'INPUT',
+    'OUTPUT',
+    'IF',
+    'ELSE',
+    'WHILE',
+}
